ea538c84c154a664bf733fe57faae251ca5e1b6c
unknown <magoyeison@gmail.com>
2019-12-28 20:53:36 -0400

    setup config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pack_1",
-    version="0.1",
+    version="1",
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
